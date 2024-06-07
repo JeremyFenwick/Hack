06fddec1b1477e7ad71ce77a4781a6b43187d07d
Jeremy Fenwick <jeremy.fenwick@gmail.com>
2024-06-07 18:44:30 +1000

    Finished fill

---
+++ Assembly/Fill.asm
@@ -0,0 +1,75 @@
+// When a key is pressed, fill the screen
+// PSEUDOCODE (C#)
+// while 
+// {
+//   if (KEYBOARD > 0)  
+//   { 
+//     var Data = -1
+//     for register in Screen
+//     {
+//       register = -1
+//     }
+          
+//   }
+// } (true)
+
+// Start the loop
+(LOOP) 
+// Check if the keyboard is currently pressed. If so go to blackscreen, else go to whitescreen
+@KBD
+D=M
+@BLACKSCREEN
+D;JGT
+@WHITESCREEN
+D;JMP
+
+(BLACKSCREEN)
+// Set the target register. When it hits the final register break the loop. 8192 registers in total
+@SCREEN
+D=A
+@targetregister
+M=D
+
+(BLACKOUT)
+// Check if we are done 
+@24576
+D=A
+@targetregister
+D=D-M
+@LOOP
+D;JLE
+// Turn the next register black
+@targetregister
+A=M
+M=-1
+
+// Add to the counter
+@targetregister
+M=M+1
+@BLACKOUT
+D;JMP
+
+(WHITESCREEN)
+// Set the target register. When it hits the final register break the loop. 8192 registers in total
+@SCREEN
+D=A
+@targetregister
+M=D
+(WHITEOUT)
+// Check if we are done 
+@24576
+D=A
+@targetregister
+D=D-M
+@LOOP
+D;JLE
+// Turn the next register white
+@targetregister
+A=M
+M=0
+
+// Add to the counter
+@targetregister
+M=M+1
+@WHITEOUT
+D;JMP
