22fb5ea407c636d5635730666f357fafb57a7fdf
Jeremy Fenwick <jeremy.fenwick@gmail.com>
2024-06-10 20:10:09 +1000

    Added assembler

---
+++ Assembler/Assembler/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Create a new default populated symbol table
+
+using Assembler;
+
+var defaultSymbolTable = new SymbolTable();
---
+++ Assembler/Assembler/SymbolTable.cs
@@ -0,0 +1,52 @@
+﻿namespace Assembler;
+
+public class SymbolTable
+{
+    private readonly Dictionary<string, int> _symbolDictionary;
+
+    public SymbolTable()
+    {
+        // Initialize the dictionary with default symbol value pairs according to the hack spec
+        _symbolDictionary = new Dictionary<string, int>
+        {
+            { "R0", 0 },
+            { "R1", 1 },
+            { "R2", 2 },
+            { "R3", 3 },
+            { "R4", 4 },
+            { "R5", 5 },
+            { "R6", 6 },
+            { "R7", 7 },
+            { "R8", 8 },
+            { "R9", 9 },
+            { "R10", 10 },
+            { "R11", 11 },
+            { "R12", 12 },
+            { "R13", 13 },
+            { "R14", 14 },
+            { "R15", 15 },
+            { "SP", 0 },
+            { "LCL", 1 },
+            { "ARG", 2 },
+            { "THIS", 3 },
+            { "THAT", 4 },
+            { "SCREEN", 16384 },
+            { "KBD", 24576 },
+        };
+    }
+
+    public void AddSymbol(string key, int value)
+    {
+        _symbolDictionary.Add(key, value);
+    }
+
+    public int GetSymbolValue(string key)
+    {
+        return _symbolDictionary[key];
+    }
+
+    public bool CheckContainsKey(string key)
+    {
+        return _symbolDictionary.ContainsKey(key);
+    }
+}
