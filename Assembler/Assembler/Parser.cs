bbf55fec2807a2029d9f2c46879140c1ca183c9a
Jeremy Fenwick <jeremy.fenwick@gmail.com>
2024-06-10 20:01:22 +1000

    Added assembler

---
+++ Assembler/Assembler/Parser.cs
@@ -0,0 +1,55 @@
+﻿namespace Assembler;
+
+public class Parser
+{
+    private List<string> _rawAssembly;
+    private Dictionary<int, string> _parsedAssembly = new();
+    private SymbolTable _symbolTable;
+    private int _codeLineIncrement = 0;
+
+    public Parser(List<String> rawAssembly, SymbolTable symbolTable)
+    {
+        _rawAssembly = rawAssembly;
+        _symbolTable = symbolTable;
+        InitalPass();
+    }
+    
+    private void InitalPass()
+    {
+        foreach (var line in _rawAssembly)
+        {
+            // Remove whitespace from line
+            var lineWithoutWhitespace = line.Replace(" ", string.Empty);
+            // Remove comments
+            var commentIndex = lineWithoutWhitespace.IndexOf("//", StringComparison.Ordinal);
+            var unCommentedLine = lineWithoutWhitespace;
+            if (commentIndex >= 0)
+            {
+                unCommentedLine = lineWithoutWhitespace.Substring(0, commentIndex);
+            }
+            // If the line is only whitespace, continue
+            if (string.IsNullOrWhiteSpace(unCommentedLine)) continue;
+            // Deal with the case where the line is a label symbol
+            if (unCommentedLine[0] == '(')
+            {
+                var value = (_codeLineIncrement);
+                var key = unCommentedLine.Substring(1, unCommentedLine.Length - 2);
+                _symbolTable.AddSymbol(key, value);
+                continue;
+            }
+            // Add the line with its index. Increment the index
+            _parsedAssembly.Add(_codeLineIncrement, unCommentedLine);
+            _codeLineIncrement++;
+        }
+    }
+
+    public Dictionary<int, String> GetParsedAssembly()
+    {
+        return _parsedAssembly;
+    }
+
+    public SymbolTable GetSymbolTableWithLabels()
+    {
+        return _symbolTable;
+    }
+}
